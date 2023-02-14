c1ad7c0e6c8f423bbd83a6bfa22a2fe4bd4fc3d8
pamelachinyemba <pamchinyemba@gmail.com>
2023-02-14 04:24:53 +0200

    Implemented add, edit,delete and exit buttons

--- gui.py
+++ gui.py
@@ -4,10 +4,20 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a todo")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button =sg.Button("Add")
+list_box = sg.Listbox(enable_events=True, values=functions.get_todos(),
+                      key="todos", size=[45, 10])
 
+edit_button = sg.Button("Edit")
+
+delete_button =sg.Button("Delete")
+
+exit_button =sg.Button("Exit")
 
 window = sg.Window("My To Do App",
-                   layout=[[label, input_box, add_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, delete_button],
+                           [exit_button]],
                    font=('Helvetica', 20))
 while True:
     event, values = window.read()
@@ -18,10 +28,31 @@ while True:
             todos = functions.get_todos()
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
-            todos = functions.write_todos(todos)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + "\n"
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(todos)
+        case 'todos':
+            window['todo'].update(values["todos"][0])
+        case 'Delete':
+            todo_to_delete = values['todos'][0]
+            todos = functions.get_todos()
+            index = todos.index(todo_to_delete)
+            todos.pop(index)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case 'Exit':
+            break
         case sg.WIN_CLOSED:
             break
-
 
 window.close()
 
